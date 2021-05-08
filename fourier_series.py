5df301d8db760318f429173231ab9b9e464c1289
muffin <squishysparrow7.7@gmail.com>
2021-05-09 09:38:13 +1200

    FOUND ISSUE! Was dividing by the len  of newpts when i shouldnt

--- fourier_series.py
+++ fourier_series.py
@@ -10,7 +10,7 @@ import math
 class Circle:
     # Values are all normal within calculations.
     # This will be used for drawing in pygame
-    unit = 5  # px
+    unit = 0.03  # px
 
     def __init__(self, screen, constant, pos,
                  circle_color=(0, 0, 0), radius_color=(255, 0, 0),
--- generate_starting_conditions.py
+++ generate_starting_conditions.py
@@ -46,7 +46,7 @@ def integral(pts, n):
         new_pts.append(
             pt * pow(math.e, n * -1 * 2 * math.pi * 1j * t)
         )
-    c = sum(new_pts)/len(new_pts)
+    c = sum(new_pts)
     return c
 
 
