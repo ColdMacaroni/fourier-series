3bb12177eea65379561ba7fd1fa9b20f02aea15f
muffin <squishysparrow7.7@gmail.com>
2021-05-09 10:00:23 +1200

    replaces exit() with inputs on argv exceptions

--- generate_starting_conditions.py
+++ generate_starting_conditions.py
@@ -94,14 +94,14 @@ def coords_to_complex(coords):
     num = coords[0] + coords[1] * 1j
     return num
 
-# TODO: Normalize values
 
+# TODO: Normalize values
 try:
     filename = argv[1]
 except IndexError:
     filename = input("Filename of svg file")
 
-cps = svg_to_readable.main(argv[1])
+cps = svg_to_readable.main(filename)
 
 
 # This will sequentially create all points.
@@ -110,16 +110,19 @@ raw_points = []
 for cp in cps:
     raw_points += generate_points(cp)
 
+# NOTE: This will be removed once normalizing values is implemented
+# The size handling and all that should be done form the
+# fourier_series.py file
+
 # Scale the svg
 try:
     factor = float(argv[3])
 except IndexError:
-    print('Argument 3 should be the scaling factor for the points')
-    exit(0)
+    factor = float(input("Enter a scale factor for the svg: "))
 
 except ValueError:
-    print('Argument 3 must be a float')
-    exit(0)
+    print("Make sure the factor is a float. E.g. 0.7")
+    factor = input("Scale factor: ")
 
 points = [(y[0] * factor, y[1] * -factor) for y in raw_points]
 
@@ -133,37 +136,28 @@ try:
     numbers = int(argv[2])
 
 except IndexError:
-    print('Argument 2 should be half the amount of circles')
-    exit(0)
+    numbers = abs(int(input("Enter the amount of pairs of circles: ")))
 
 except ValueError:
-    print('Argument 2 must be an int')
-    exit(0)
+    numbers = abs(int(input("Enter the amount of pairs of circles. "
+                            "Must be an integer: ")))
 
 for n in range(0, numbers + 1):
+    # Generate the circles
     constants.append(integral(points, n))
 
+    # Create an "opposite" circle right next to
+    # the current one
     if n != 0:
         constants.append(integral(points, n * -1))
 
-#for j in constants:
-#    print(j)
-
-
-# # The average of the points
-# # See https://github.com/ColdMacaroni/pygame-bezier
-# avg_pt = (0.5593747196325674, 0.5946344182531562)
-#
-# # Calculate the value c0
-# static = coords_to_complex(avg_pt)
-#
-# constants = []
-#
-# # Amount of circles
-# amount = 400
-# for n in range(0, amount + 1):
-#     const = static * pow(math.e, (n * -1) * 2 * math.pi * 1j)
-#     constants.append(const)
+# By the end, the values would look like
+# 0
+# 1
+# -1
+# 2
+# -2
+# etc.s
 
 # Clear file
 filename = 'constants'
