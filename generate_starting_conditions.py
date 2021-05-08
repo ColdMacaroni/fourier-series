5c5d97dcce6ca0d0ced77b229f76a42c557f9a86
muffin <squishysparrow7.7@gmail.com>
2021-05-08 21:18:58 +1200

    start on script

--- generate_starting_conditions.py
+++ generate_starting_conditions.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 # Generate starting sequences for fourier series
+
+def coords_to_complex(coords):
+    num = coords[0] + coords[1] * 1j
+    return num
+
+file = open('constants', 'a')
+
+avg_pt = (-13.399837757, 25.824553376666657)
+
+file.write(coords_to_complex(avg_pt))
