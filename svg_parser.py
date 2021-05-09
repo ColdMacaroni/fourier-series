90a18b7633ffb5d6f3ccfc9f75bab2f19672dc4d
muffin <squishysparrow7.7@gmail.com>
2021-05-09 21:30:21 +1200

    lots of processing functions that are working

--- svg_parser.py
+++ svg_parser.py
@@ -22,35 +22,119 @@ def get_d(filename):
     return ctrl_points
 
 
-def tuplify_d(ls):
+def list_d(ls):
     """
-    Converts strings like "print(processed_list)0.4,-34" to (0.4, -34)
+    Turns d into a list
     """
     # Processed items will go in here
     new_ls = []
 
+    # Support for when the coordinates are divided by commas
+    ls = ls.replace(",", " ")
     items = ls.split(" ")
+
     for item in items:
         # Leave string items as they are
         if item.isalpha():
             new_ls.append(item)
 
-        # Convert to tuple
+        # Convert nums to actual nums
         else:
-            try:
-                x, y = item.split(",")
-
-                # Convert to a tuple with actual nums
-                coordinate = float(x.strip()), float(y.strip())
-
-            # There will be a value error if the item is just a number
-            # without commas
-            # This will happen when the value is from a horizontal or
-            # vertical lineto
-            except ValueError:
-                coordinate = float(item.strip())
+            coordinate = float(item.strip())
 
             new_ls.append(coordinate)
+
+    return new_ls
+
+
+def tuplify_d(ls):
+    """
+    Convert a list given by sep_commands into using coordinates
+    in tuples instead of standalone
+    """
+    new_ls = []
+    for item in ls:
+        print(item)
+        new_item = [item[0]]
+
+        # Go 2 by 2 to add the coords together
+        for pos in range(1, len(item), 2):
+            # Convert to tuple
+            coord = item[pos], item[pos + 1]
+
+            new_item.append(coord)
+
+        print(new_item)
+        print()
+
+        new_ls.append(new_item)
+    return new_ls
+
+
+def sep_commands(ls):
+    """
+    Creates 2D lists for each svg command
+    """
+    new_ls = []
+    last = 0
+    for i in range(len(ls)):
+        # When it encounters a string, it will add the previous
+        # section to the new list
+        if isinstance(ls[i], str):
+            to_append = ls[last:i]
+            # Only add if the list is not empty
+            if to_append:
+                new_ls.append(to_append)
+                last = i
+
+    print(new_ls, "sep_commands")
+    return new_ls
+
+
+def relative_to_absolute(sep_ls):
+    """
+    Taking a list generated from sep_commands(), convert
+    the values to absolute ones
+    """
+    pass
+
+
+def separate_points(sep_ls):
+    """
+    Taking a list generated from sep_commands(), it'll convert
+    the commands to separate continuous ones
+    """
+    # Shortcuts
+    horiz = lambda x: ['l', [(x, 0)]]
+    verti = lambda y: ['l', [(0, y)]]
+
+    # The minimum amount of attributes each has
+    # coms is short for commands
+    coms = {
+        "m": 1,  # Move
+
+        "l": 1,  # Line to
+        "h": 1,  # Horizontal
+        "v": 1,  # Vertical
+
+        "c": 3,  # Cubic bezier
+        "s": 2,  # Smooth cubic bezier
+
+        "q": 2,  # Quadratic bezier
+    }
+
+    new_ls = []
+    for item in sep_ls:
+        com = item[0].lower()
+        start = coms[com]
+        for i in range(1, len(item), coms[com]):
+            # Do this to avoid appending empty lists
+            to_append = item[start:i]
+            if to_append:
+                new_ls.append([item[0], to_append])
+
+            start = i
+
     return new_ls
 
 
@@ -58,13 +142,19 @@ def main(filename):
     # Get the content of d
     d_markers = get_d(filename)
 
-    # Convert d to actual, usable data types
-    processed_list = tuplify_d(d_markers)
+    # Hehe
+    d_list = list_d(d_markers)
 
-    
+    # Group per command type
+    separated_list = sep_commands(d_list)
 
+    processed_list = tuplify_d(separated_list)
 
+    # Convert shorthand commands to full ones
+    command_list = separate_points(processed_list)
+
+    return command_list
 
 
 if __name__ == "__main__":
-    main(argv[1])
+    print(main(argv[1]))
