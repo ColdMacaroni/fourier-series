d88e139a3b7185da3ee83ebc0407570294e22973
muffin <squishysparrow7.7@gmail.com>
2021-05-09 18:33:10 +1200

    Func to convert the d to good data types

--- svg_parser.py
+++ svg_parser.py
@@ -4,9 +4,13 @@
 import bs4 as bs
 from sys import argv
 
+
 def get_d(filename):
-# Replace argv with filename etc
-    source = open(argv[1], 'r')
+    """
+    Gets the contents of the attribute d of the first path
+    it finds in the given svg file
+    """
+    source = open(filename, 'r')
 
     soup = bs.BeautifulSoup(source, 'xml')
 
@@ -18,8 +22,40 @@ def get_d(filename):
     return ctrl_points
 
 
+def tuplify_coords(ls):
+    """
+    Converts strings like "0.4,-34" to (0.4, -34)
+    """
+    # Processed items will go in here
+    new_ls = []
+
+    items = ls.split(" ")
+    for item in items:
+        # Leave string items as they are
+        if item.isalpha():
+            new_ls.append(item)
+
+        # Convert to tuple
+        else:
+            x, y = item.split(",")
+
+            # Convert to a tuple with actual nums
+            coordinate = float(x.strip()), float(y.strip())
+
+            new_ls.append(coordinate)
+
+    return new_ls
+
+
 def main(filename):
-    pass
+    # Get the content of d
+    d_markers = get_d(filename)
+
+    processed_list = tuplify_coords(d_markers)
+
+    print(processed_list)
+
+
 
 if __name__ == "__main__":
-    main(argv[0])
+    main(argv[1])
